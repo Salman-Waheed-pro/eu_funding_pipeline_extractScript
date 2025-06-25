0f0695cc7919e464c5e73d12713d579ce86015b5
Salman-Waheed-pro <149319612+Salman-Waheed-pro@users.noreply.github.com>
2025-06-25 15:32:46 +0300

    imported tempfile

--- tasks/extract.py
+++ tasks/extract.py
@@ -1,6 +1,7 @@
 import luigi
 import json
 import time
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
